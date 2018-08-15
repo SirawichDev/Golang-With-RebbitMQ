cb37fe628c193597d701df20c1f1710214a8076f
SirawichDev <sirawit0676@gmail.com>
2018-08-15 14:52:15 +0700

    first RabbitMQ commit

---
+++ src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	server()
+}
+func server() {
+	conn, ch, q := getQueue()
+	defer conn.Close()
+	defer ch.Close()
+
+	msg := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("Hellow Me "),
+	}
+	for {
+		ch.Publish("", q.Name, false, false, msg)
+	}
+}
+
+func getQueue() (*amqp.Connection, *amqp.Channel, *amqp.Queue) {
+	conn, err := amqp.Dial("amqp://guest@localhost:5672")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open Channel")
+	q, err := ch.QueueDeclare(
+		"Hellow",
+		false,
+		false,
+		false,
+		false,
+		nil)
+	failOnError(err, "Failed to declare a queue")
+	return conn, ch, &q
+}
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalln(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
